b2a8ee3d3e39cd9641adbd2d0db13970e6bbe081
philiptavaresvs@gmail.com <philiptavaresvs@gmail.com>
2021-05-21 15:28:10 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/atendimentos', (req, res) => {
+    return res.send('Voce está na rota de atendimentos /GET');
+})
+
+app.listen(3000, ()=>console.log('Servidor na porta 3000'));
