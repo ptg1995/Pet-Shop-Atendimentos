dd5260bca307381d414b6af05f52bc0578a7edce
philiptavaresvs@gmail.com <philiptavaresvs@gmail.com>
2021-05-23 19:54:20 -0300

    SelectByName

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -3,12 +3,13 @@ const AtendimentosDac = require('../models/atendimentosDAC')
 const dac = new Atendimento();
 module.exports = app => {
     app.get('/atendimentos', (req, res) => {
-        return res.send('Voce está na rota de atendimentos /GET');
+        dac.SelectConsultas(res);
     });
+    app.get('/atendimentos/:cliente', (req, res) => {
+        dac.SelectConsultasByName(req.params['cliente'],res);
+    })
     app.post('/atendimentos', (req, res) => {
         console.log(req.body)
         dac.InsertAtendimentos(req.body, res);
-        //console.log(res);
-       // return res.end(JSON.stringify('Atendimento agendado com sucesso!',req.body))
-    })
+    });
 }
--- models/atendimentosDAC.js
+++ models/atendimentosDAC.js
@@ -5,8 +5,8 @@ const { response } = require('express');
 class Atendimento{
     InsertAtendimentos(atendimento, res){
         const dataCriacao = new Date();
-        const dataCriacaoFormat = moment(dataCriacao.getTime()).format('YYYY-MM-DD HH:mm:ss')
-        const  dataAtendimentoFormat = moment(atendimento.dataAtendimento, 'DD-MM-YYYY HH:mm:ss').format('YYYY-MM-DD HH:mm:ss')
+        const dataCriacaoFormat = moment(dataCriacao.getTime()).format('YYYY-MM-DD HH24:mm:ss')
+        const  dataAtendimentoFormat = moment(atendimento.dataAtendimento, 'DD-MM-YYYY HH24:mm:ss').format('YYYY-MM-DD HH24:mm:ss')
         //valida data do atendimento
         if(dataAtendimentoFormat<= dataCriacaoFormat ){
             return res.status(400).send('A data da consulta deve ser maior que a data atual.')
@@ -27,6 +27,41 @@ class Atendimento{
                 res.status(201).json(content);
             }
         });
+    };
+    SelectConsultas( res){
+        const sql = (`Select cliente, pet, servico, observacoes, to_char(dth_consulta, 'YYYY-MM-DD HH24:mm:ss') as dth_consulta, to_char(dth_marcacao, 'YYYY-MM-DD HH24:mm:ss') as dth_marcacao from public.atendimentos`) 
+        conexao.query(sql,(err, content) =>{ 
+            if(err) {
+                res.status(400).json(err)
+            }
+            else{
+                res.status(201).json(content.rows);
+            }
+        });
+    };
+    SelectConsultasByName(cliente,res){
+        console.log(cliente)
+        const sql = (`Select cliente, pet, servico, observacoes, to_char(dth_consulta, 'YYYY-MM-DD HH24:mm:ss') as dth_consulta, to_char(dth_marcacao, 'YYYY-MM-DD HH24:mm:ss') as dth_marcacao from public.atendimentos WHERE cliente = '${cliente}'`) // or pet = '${req.params.pet}' or  dth_consulta = ${req.params.dataAtendimento}`)    
+        conexao.query(sql,(err, content) =>{ 
+            if(err) {
+                res.status(400).json(err)
+            }
+            else{
+                res.status(201).json(content.rows[0]);
+            }
+        });
+    };
+    UpDateByName(name, res){
+        const sql = (`Select cliente, pet, servico, observacoes, to_char(dth_consulta, 'YYYY-MM-DD HH24:mm:ss') as dth_consulta, to_char(dth_marcacao, 'YYYY-MM-DD HH24:mm:ss') as dth_marcacao from public.atendimentos WHERE cliente = '${cliente}'`) // or pet = '${req.params.pet}' or  dth_consulta = ${req.params.dataAtendimento}`)    
+        conexao.query(sql,(err, content) =>{ 
+            if(err) {
+                res.status(400).json(err)
+            }
+            else{
+                res.status(201).json(content.rows[0]);
+            }
+        });
     }
+
 }
 module.exports = Atendimento;
